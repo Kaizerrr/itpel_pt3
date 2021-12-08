8f86bf03712b5124deeaa48e4d8841d37d487a69
John Michael E. Castillo <jmc6740@gmail.com>
2021-12-09 05:01:47 +0800

    commit

---
+++ src/app/api.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+  headers: HttpHeaders;
+
+  constructor(public http: HttpClient){
+    this.headers = new HttpHeaders();
+    this.headers.append('Accept', 'application/json');
+    this.headers.append('Content-Type', 'application/json');
+    this.headers.append('Access-Control-Allow-Origin', '*');
+
+
+  }
+
+  addStudent(data){
+    return this.http.post('http://localhost/ionic/backend/create.php', data);
+
+  }
+}
+
